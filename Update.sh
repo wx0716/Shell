8cb667eab151ada405ae37b52b5bcab9cbe76a87
王鑫 <wx071611@icloud.com>
2019-10-13 16:56:00 +0800

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 sudo apt full-upgrade
+
+echo '更新成功，已成功退出！'
+
 sudo apt autoremove
-sudo apt install curl && sudo apt install wget
+
+echo '删除成功，已成功退出！'
+
+sudo apt install curl 
+
+echo '更新成功，已成功退出！'
+
+sudo apt install wget
 
 echo '更新成功，已成功退出！'
