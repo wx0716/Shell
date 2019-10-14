bb6c8c6c2a9bd427c4c23836a47ca5a1785f6261
王鑫 <wx071611@icloud.com>
2019-10-14 10:02:51 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# author：王鑫
 sudo apt full-upgrade
 
 echo '更新成功，已成功退出！'
@@ -14,3 +15,4 @@ echo '更新成功，已成功退出！'
 sudo apt install wget
 
 echo '更新成功，已成功退出！'
+echo '恭喜你已经完成了系统更新，谢谢您使用此脚本！'
