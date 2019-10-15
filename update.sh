97f36f7e5007fb43fa5997b33892465997e0cfb3
王鑫 <wx071611@icloud.com>
2019-10-15 23:33:15 +0800

    系统一键更新脚本

--- update.sh
+++ update.sh
@@ -2,17 +2,26 @@
 # author：王鑫
 sudo apt full-upgrade
 
+echo ''
 echo '更新成功，已成功退出！'
+echo ''
+
 
 sudo apt autoremove
 
+echo ''
 echo '删除成功，已成功退出！'
+echo ''
 
 sudo apt install curl 
 
+echo ''
 echo '更新成功，已成功退出！'
+echo ''
 
 sudo apt install wget
 
+echo ''
 echo '更新成功，已成功退出！'
+echo ''
 echo '恭喜你已经完成了系统更新，谢谢您使用此脚本！'
