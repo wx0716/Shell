9549d37ca241b0746c193f9e435234b323d1ecbf
王鑫 <wx@wx0716.cn>
2019-12-11 15:30:13 +0800

    rewrite update.sh

--- update.sh
+++ update.sh
@@ -1,51 +1,44 @@
-#!/bin/bash
-# author:王鑫
-# author_email:wx071611@gmail.com
+#! /usr/bin/bash
+# author: David wang
+# author_email: wx@wx0716.cn
+# author_blog: www.wx0716.org
 
 echo "……欢迎使用此脚本，脚本开始运行……"
 echo ""
 
 echo "开始更新系统组件，若没有则自动跳过!"
 echo ""
-sudo apt update && sudo apt upgrade
-echo ""
+
+sudo apt update
+sudo apt upgrade
+echo " "
 
 echo "开始检测是否安装了curl，若无则安装如有则更新!"
-echo ""
-curl --version && sudo apt install curl
-echo ""
+echo " "
+curl --version
+sudo apt install curl
+echo " "
 
 echo "开始检测是否安装了wget，若无则安装如有则更新!"
-echo ""
-wget --version && sudo apt install wget
-echo ""
-
-echo "开始检测是否安装了node-js环境，若无则安装如有则更新!"
-echo ""
-node --version && sudo apt install nodejs
-echo ""
-
-echo "开始检测是否安装了npm，若无则安装如有则更新!"
-echo ""
-npm --version && sudo apt install npm && npm install -g npm
-echo ""
-
-echo "开始检测是否安装了gcc和cmake，若无则安装如有则更新!"
-echo ""
-gcc --version && sudo apt install gcc && cmake --version && sudo apt install cmake
-echo ""
+echo " "
+wget --version
+sudo apt install wget
+ehco " "
 
 echo "开始检测是否安装了python3和pip，若无则安装如有则更新!"
-echo ""
-python3 --version && sudo apt install python3
-echo ""
-pip3 --version && sudo apt install python3-pip && pip3 install --upgrade pip
-echo ""
+echo " "
+python3 --version 
+sudo apt install python3
+ehco " "
+pip3
+sudo apt install python3-pip
+pip3 install --upgrade pip
+ehco " "
 
 echo "开始清除垃圾文件……"
-echo ""
+echo " "
 sudo apt autoremove
-echo ""
+echo " "
 
 echo "一键更新全部更新安装完毕，感谢您使用此脚本，欢迎再次使用此脚本。"
 ehco ""
