d2b6ca7857674c9b57a7ccab7afb30cfaf102273
王鑫 <wx@wx0716.cn>
2019-12-11 16:07:20 +0800

    update update.sh

--- update.sh
+++ update.sh
@@ -26,7 +26,6 @@ echo ""
 wget --version
 
 sudo apt install -y wget
-ehco ""
 
 echo "开始检测是否安装了python3和pip，若无则安装如有则更新!"
 echo ""
@@ -39,7 +38,6 @@ pip3
 sudo apt install -y python3-pip
 
 pip3 install --upgrade pip
-ehco ""
 
 echo "开始清除垃圾文件……"
 echo " "
@@ -51,4 +49,5 @@ sudo apt autoclean -y
 echo ""
 
 echo "一键更新全部更新安装完毕，感谢您使用此脚本，欢迎再次使用此脚本。"
+echo "当然也欢迎大家访问我的博客网站:www.王鑫0716.com,或者GitHub主页:www.github.com/wx0716"
 echo "如需再次使用此脚本，只需输入bash update.sh命令即可再次使用。"
