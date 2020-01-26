cc353323500b7e1bf6c75a8e3142fb6a3b72c05b
wx0716 <wx@wx0716.cn>
2020-01-26 20:18:36 +0800

    CentOS 一键系统更新脚本

--- centos.sh
+++ centos.sh
@@ -6,6 +6,8 @@
 echo "……欢迎使用此脚本，脚本开始运行……"
 echo ""
 
+sudo -i
+
 echo "开始更新系统组件，若没有则自动跳过!"
 echo ""
 
@@ -16,20 +18,20 @@ echo ""
 echo "开始检测是否安装了curl，若无则安装如有则更新!"
 echo ""
 curl --version
-sudo yum -y install curl && sudo yum -y update curl
+yum -y install curl && yum -y update curl
 echo ""
 
 echo "开始检测是否安装了wget，若无则安装如有则更新!"
 echo ""
 wget --version
-sudo yum -y install wget && sudo yum -y update wget
+yum -y install wget && yum -y update wget
 ehco ""
 
 echo "开始清除垃圾文件……"
 echo ""
-sudo yum -y autoremove
-sudo yum -y clean all
+yum -y autoremove
+yum -y clean all
 echo ""
 
 echo "一键更新全部更新安装完毕，感谢您使用此脚本，欢迎再次使用此脚本。"
-echo "如需再次使用此脚本，只需输入bash centos_update.sh命令即可再次使用。"
+echo "如需再次使用此脚本，只需输入bash centos.sh命令即可再次使用。"
