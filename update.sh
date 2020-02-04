c7a02cf5170f481b438db55739813d827b152122
wx0716 <wx@wx0716.org>
2020-02-04 17:10:10 +0800

    Ubuntu系统一键更新脚本

--- update.sh
+++ update.sh
@@ -3,62 +3,35 @@
 # author_email: wx@wx0716.org
 # author_blog: www.wx0716.org
 
-printf "……欢迎使用此脚本，脚本开始运行……"
-
-printf "开始更新系统组件，若没有则自动跳过!"
-
+echo "……欢迎使用此脚本，脚本开始运行……"
+echo "开始更新系统组件，若没有则自动跳过!"
 apt update -y
-
-printf ""
-
+echo ""
 apt upgrade -y
-
-printf "开始检测是否安装了curl，若无则安装如有则更新!"
-
+echo "开始检测是否安装了curl，若无则安装如有则更新!"
 curl --version
-
-printf ""
-
+echo ""
 apt install -y curl
-
-printf "开始检测是否安装了wget，若无则安装如有则更新!"
-
+echo "开始检测是否安装了wget，若无则安装如有则更新!"
 wget --version
-
-printf ""
-
+echo ""
 apt install -y wget
-
-printf "开始检测是否安装了python3和pip，若无则安装如有则更新!"
-
+echo "开始检测是否安装了python3和pip，若无则安装如有则更新!"
 python3 --version 
-
-printf ""
-
+echo ""
 apt install -y python3
-
-printf ""
-
+echo ""
 pip3
-
-printf ""
-
+echo ""
 apt install -y python3-pip
-
-printf ""
-
+echo ""
 pip3 install --upgrade pip
-
-printf "开始清除垃圾文件……"
-
+echo "开始清除垃圾文件……"
 apt autoremove -y
-
-printf ""
-
+echo ""
 apt autoclean -y
-
-printf "一键更新全部更新安装完毕，感谢您使用此脚本，欢迎再次使用此脚本。"
-printf ""
-printf "当然也欢迎大家访问我的博客网站:https://www.wx0716.org"
-printf ""
-printf "如需再次使用此脚本，只需输入bash update.sh命令即可再次使用。"
+echo "一键更新全部更新安装完毕，感谢您使用此脚本，欢迎再次使用此脚本。"
+echo ""
+echo "当然也欢迎大家访问我的博客网站:https://www.wx0716.org"
+echo ""
+echo "如需再次使用此脚本，只需输入bash update.sh命令即可再次使用。"
