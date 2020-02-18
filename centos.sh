81252f3b6b37bf613e2714f2da034ede80fad49d
王鑫 <wx@wangxindeMacBook-Air.local>
2020-02-19 01:36:11 +0800

    CentOS系统一键更新脚本

--- centos.sh
+++ centos.sh
@@ -4,31 +4,59 @@
 # author_blog: www.wx0716.org
 
 echo "……欢迎使用此脚本，脚本开始运行……"
+
 echo "开始更新系统组件，若没有则自动跳过!"
+
 yum -y upgrade
+
 echo "开始检测是否安装了curl，若无则安装如有则更新!"
+
 curl --version
+
 echo ""
+
 yum -y install curl && yum -y update curl
+
 echo "开始检测是否安装了wget，若无则安装如有则更新!"
+
 wget --version
+
 echo ""
+
 echo "开始检测是否安装了pip3"
+
 pip3
+
 echo ""
+
 echo "开始安装pip3"
+
 yum -y install python3-pip
+
 echo ""
+
 echo "开始更新pip3"
+
 pip3 install --upgrade pip
+
 echo ""
+
 yum -y install wget && yum -y update wget
+
 echo "开始清除垃圾文件……"
+
 yum -y autoremove
+
 echo ""
+
 yum -y clean all
+
 echo "一键更新全部更新安装完毕，感谢您使用此脚本，欢迎再次使用此脚本。"
+
 echo ""
+
 echo "如需再次使用此脚本，只需输入bash centos.sh命令即可再次使用。"
+
 echo ""
-echo "欢迎大家访问我的主页https://www.wx0716.cn"
+
+echo "欢迎大家访问我的主页https://www.wx0716.org"
